9ef90f1048e698c56f25047219460a472bc16969
K4nade <git@k4na.de>
2023-06-19 23:17:03 +0900

    add part4

--- Part4_Standard_IO/main.c
+++ Part4_Standard_IO/main.c
@@ -1,1 +1,19 @@
+// get input and return output
+# include <stdio.h>
 
+// hello world
+int main(void)
+{
+    // get two numbers from scanf input
+    int x, y;
+    printf("x: ");
+    scanf("%i[0-9]", &x);
+    printf("y: ");
+    scanf("%i[0-9]", &y);
+
+    // print two numbers each on a new line
+    printf("x: %i\n", x);
+    printf("y: %i\n", y);
+
+    return 0;
+}
