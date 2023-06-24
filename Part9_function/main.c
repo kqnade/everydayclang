4621a19896563db21e98f964a341c5d620133adf
K4nade <git@k4na.de>
2023-06-24 13:28:01 +0900

    make part 9

--- Part9_function/main.c
+++ Part9_function/main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
+int get_sum(int a, int b) {
+    return a + b;
+}
+
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    int x = 1;
+    int y = 2;
+    int z = get_sum(x, y);
+    printf("Sum of %d and %d is %d\n", x, y, z);
 }
